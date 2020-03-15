4d4fa3e6fad22216d2398cd83d6feb9145e5f27c
Melissa Astbury <melissajastbury@gmail.com>
2020-03-15 18:01:58 +0000

    adding in JS. if statements and ES6

--- quiz.js
+++ quiz.js
@@ -1,3 +1,26 @@
-function check() {
-  alert("you won!");
-}
+const check = () => {
+  let score = 0;
+  const inputbox = document.getElementById("textbox").value;
+  const radioAnswers = document.querySelector(
+    'input[name="questionTwo"]:checked'
+  ).value;
+  const chosenColor = document.getElementById("colors").selectedIndex;
+  const hiddenDiv = document.getElementById("hiddenDiv");
+  const hiddenText = document.getElementById("hiddenDiv");
+
+  // inputbox === "London" || inputbox === "london"
+  if (inputbox.toLowerCase() === "london") {
+    score += 3;
+  }
+  if (radioAnswers === "spain") {
+    score += 3;
+  }
+  if (chosenColor === 0) {
+    score += 3;
+  }
+
+  hiddenText.textContent = `You scored ${score}`;
+  hiddenDiv.style.display = "block";
+
+  //alert(`You're score is ${score}`);
+};
